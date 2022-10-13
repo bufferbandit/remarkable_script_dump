8bb9c1a5e458bcecd2c6491f71b6cb9847c23147
kernelland <kernelland@protonmail.ch>
2022-10-13 19:21:08 +0200

    Create use_framebuffer.sh

---
+++ use_framebuffer.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+
+if [ -z "$NO_XO" ]; then
+    systemctl stop xochitl
+fi
+
+if [ ! -z "`pidof rm2fb-server`"]; then
+    killall rm2fb-server
+fi
+
+LD_PRELOAD=/home/root/librm2fb_server.so.1.0.1 /usr/bin/xochitl &
+sleep 2
+
+#export KO_DONT_SET_DEPTH=1
+#export KO_DONT_GRAB_INPUT=1
+export LD_PRELOAD=/home/root/librm2fb_client.so.1.0.1
+
+eval $1
+
+killall rm2fb-server
+
+systemctl reset-failed xochitl
+if [ -z "$NO_XO" ]; then
+    systemctl start xochitl
+fi
